b5516e094b05937682ba973e3f1313902e11a5c3
Alejandro <ealejandrorg@gmail.com>
2021-12-18 03:12:15 +0100

    PHP 8.1 Image 🚀

---
+++ tests/test.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+ini_set('assert.exception', 'true');
+
+// memory limit
+assert(ini_get('memory_limit') === "-1");
+
+// extensions
+assert(extension_loaded('intl'));
+assert(extension_loaded('zip'));
+assert(extension_loaded('json'));
+assert(extension_loaded('bcmath'));
+assert(extension_loaded('pdo_mysql'));
+assert(extension_loaded('pdo'));
+assert(extension_loaded('apcu'));
+assert(extension_loaded('amqp'));
+assert(extension_loaded('mbstring'));
+
